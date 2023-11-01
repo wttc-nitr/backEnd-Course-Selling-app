dc64631bcec7b1b3bdfed284143eb48b60dcd6c3
Animesh Kumar <kumaranimesh097@gmail.com>
2023-11-01 15:21:21 +0530

    add dotenv

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import mongoose from "mongoose"
 import adminRouter from "./server/routes/admin" 
 import userRouter from "./server/routes/user"
+import 'dotenv/config'
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
 mongoose.connect(
-  ""
+  `${process.env.MONGO_URL}`
 );
 
 app.listen(3000, () => {
