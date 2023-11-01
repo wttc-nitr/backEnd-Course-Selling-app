92fc6f84b00d32a97dcfd814ca5170128570f9db
Animesh Kumar <kumaranimesh097@gmail.com>
2023-11-01 15:25:50 +0530

    ...

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,13 @@ const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const admin_1 = __importDefault(require("./server/routes/admin"));
 const user_1 = __importDefault(require("./server/routes/user"));
+require("dotenv/config");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/admin", admin_1.default);
 app.use("/user", user_1.default);
-mongoose_1.default.connect("");
+mongoose_1.default.connect(`${process.env.MONGO_URL}`);
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
